acdcaeb41192fb3e7bcf508a506dda706bb63716
Eduardo Estévez <alu0101014319@ull.edu.es>
2019-11-13 23:16:48 +0000

    Alimento   Obtener el nombre   Obtener gramos de proteína   Obtener gramos de carbohidratos   Obtener gramos de lípidos   Obtener kilos de gases de efecto invernadero   Obtener uso del terreno anual en m²   Obtener el alimento formateado   Obtener las kcal del alimento   Calcular el impacto ambiental de la dieta recomendada para un varón adulto (54gr proteína)     Obtener impacto ambiental de la dieta   Calcular el impacto ambiental de la dieta recomendada para una mujer adulta (47 gr proteína)     Obtener el impacto ambiental de la nueva dieta
    Finished in 0.00437 seconds (files took 0.13328 seconds to load)
    10 examples, 0 failures
    
    Fin de la práctica

--- spec/feeding_spec.rb
+++ spec/feeding_spec.rb
@@ -88,6 +88,20 @@ RSpec.describe Alimento do
 	       end
        end
 
+       context "Calcular el impacto ambiental de la dieta recomendada para una mujer adulta (47 gr proteína)" do
+	       it "Obtener el impacto ambiental de la nueva dieta" do
+		       @carne_de_vaca = Alimento.new("Carne de vaca",10.0,0.0,1.47,50.0,164.0) #47.4 gr
+                       @carne_de_cordero = Alimento.new("Carne de cordero",10.0,0.0,9.44,20.0,185.0) #55.6 gr
+                       @lentejas_ = Alimento.new("Lentejas",10.0,22.13,0.6,0.4,3.4) #42.6 gr
+                       @tofu = Alimento.new("Tofu",10.0,2.38,6.0,2.0,2.2) #125 gr
+		       @nuez = Alimento.new("Nuez",7.0,7.35,18.9,0.3,7.9) #35 gr
+
+		       expect(@carne_de_vaca.impacto(47.4,@carne_de_cordero,55.6, @lentejas,42.6, @tofu,125, @nuez,35).to_s).to eq("[3.76, 18.76]")
+
+	       end
+       end
+
+
 
 
 
