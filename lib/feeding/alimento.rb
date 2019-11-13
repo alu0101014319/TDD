56789664a7ed697674aae49a227199349aa9485d
Eduardo Estévez <alu0101014319@ull.edu.es>
2019-11-13 20:22:35 +0000

    Alimento   Instanciando objetos (FAILED - 1)
    Failures:
    
      1) Alimento Instanciando objetos
         Failure/Error: @carne_vaca = Alimento.new("Carne de vaca", 21.1,0.0,3.1,50.0,164.0)
    
         NameError:
           uninitialized constant Alimento
         # ./spec/feeding_spec.rb:3:in `block (2 levels) in <top (required)>'
    
    Finished in 0.00158 seconds (files took 0.12759 seconds to load)
    1 example, 1 failure
    
    Failed examples:
    
    rspec ./spec/feeding_spec.rb:2 # Alimento Instanciando objetos

--- lib/feeding/alimento.rb
+++ lib/feeding/alimento.rb
@@ -1,3 +1,12 @@
 class Alimento
+	attr_reader :no, :pr, :ca, :li, :gei, :te 
+	def initialize(no,pr,ca,li,gei,te)
+		@no = no
+		@pr = pr
+		@ca = ca
+		@li = li
+		@gei = gei
+		@te = te
+	end
 
 end
--- spec/feeding_spec.rb
+++ spec/feeding_spec.rb
@@ -1,3 +1,10 @@
 RSpec.describe Alimento do
+	it "Instanciando objetos" do
+                @carne_vaca = Alimento.new("Carne de vaca", 21.1,0.0,3.1,50.0,164.0)
+		@carne_cordero = Alimento.new("Carne de cordero", 18.0,0.0,17.0,20.0,185.0)
+		@camarones = Alimento.new("Camarones", 17.6,1.5,0.6,18.0,2.0)
+		@salmon = Alimento.new("Salmón",19.9,0.0,13.6,6.0,3.7)
+		@lentejas = Alimento.new("Lentejas",23.5,52.0,1.4,0.4,3.4)
+        end
 
 end
